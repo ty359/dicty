31cd2ab3076ca8f15196bfaad5e6b86b7c50da57
__ty359 <ty359@qq.com>
2022-09-22 16:06:13 +0800

    fix sample

--- sample.cc
+++ sample.cc
@@ -1,10 +1,12 @@
-#include "dicty.hpp"
+#include "dicty.h"
 
 int main() {
     Dicty::Dicty d;
     d.Compile({"0", "133", "axx", "asjdasdjads", "hello world!"});
-    printf("%d\n", d.Get("133"));
-    printf("%d\n", d.Get("hello world!"));
-    printf("%d\n", d.Get("asjdasdjads"));
+    printf("%d\n", d.Get("133"));           // get 1
+    printf("%d\n", d.Get("hello world!"));  // get 4
+    printf("%d\n", d.Get("asjdasdjads"));   // get 3
+    printf("%d\n", d.Get("axx"));           // get 2
+    printf("%d\n", d.Get("0"));             // get 0
     return 0;
 }
